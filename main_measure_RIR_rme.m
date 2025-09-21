261bec141d5e773e1f09ddde9abb6552059e7bc8
Antonio Figueroa-Duran <anfig@elektro.dtu.dk>
2025-09-21 20:31:02 +0200

    Measure single RIRs routine (for RT estimation)

---
+++ main_measure_RIR_rme.m
@@ -0,0 +1,97 @@
+%% ==== Clear workspace ====
+clear, clc, close all
+
+%% ==== Parameters ====
+% RIR: measurement parameters
+T = 3;          % Sweep length [s]
+Toff = 1;       % Silence length = RIR length [s]
+fs = 48e3;      % Sampling frequency [Hz]
+Fband = [20 20E3];  % Sweep bandwidth
+Nsamples = fs*(T+Toff); % A-priori number of samples
+gain_nexus = 1;    % Nexus gain [V/Pa]
+gain_sweep = -7;        % Sweep gain
+frameSize = 512;        % Frame size to minimise latency
+maxRep = 2;             % Max repetitions in case of samples over/underrun
+numMicPos = 6;          % Num microphone positions per source position
+numSourcePos = 3;       % Num source positions
+
+% RIR: folder and file structure
+folderData = 'Data/Kitchen/RT/';
+fileNamePrefix = 'single_RIR_';
+
+% Room conditions
+roomDimensions = [6.7 6.25 3.0]; % Room dimensions [m x m x m]
+tempC = 24.2;       % Temperature [C]
+humidityRH = 61.4;  % Relative humidity [%RH]
+
+%% ==== Connect to SOUNDCARD ====
+infoDev = audiodevinfo;
+nameDev = 'ASIO Fireface USB';
+% infoDev.output.Name
+
+playRec = audioPlayerRecorder(fs,"SupportVariableSize",true,...
+    Device=nameDev,...
+    BufferSize=frameSize,...
+    BitDepth="24-bit integer");
+
+% Mic Channel - 5
+playRec.RecorderChannelMapping = 5;
+
+% Loudspeaker Channel - 5
+playRec.PlayerChannelMapping = 1;
+
+%% ==== Create exponential sweep ====
+% Adapt sweep length to obtain full frames (initial RIR length is modified)
+nFrames = ceil(Nsamples/frameSize);
+Nsamples = nFrames*frameSize;
+Toff = Nsamples/fs - T;
+Nh = fs*Toff;
+t = 0:1/fs:Toff-1/fs;
+
+% Gen sweep
+sweep = sweeptone(T,Toff,fs,'SweepFrequencyRange',Fband,'ExcitationLevel',gain_sweep);
+
+% Slice sweep
+audioToPlay = reshape(sweep,frameSize,Nsamples/frameSize);
+
+%% ==== Run measurements ====
+% Check for folder
+if ~exist(folderData,'dir')
+    mkdir(folderData)
+end
+
+% Save metadata
+% save([folderData 'metadata'])
+
+figure, hold on
+for sPos = 2:numSourcePos
+    disp(['---- Source Position ' num2str(sPos) ' -----'])
+
+    for mPos = 1:numMicPos
+        disp(['Mic Position ' num2str(mPos) ' -----'])
+
+        disp('Ready?'), pause
+
+        % ---- measure RIR ---------------------------------------------------
+        % Play + record signal
+        [p_meas,flagRep] = get_dataRME(playRec,audioToPlay,maxRep);
+        if flagRep == 1, disp(['ERROR: Max repetition number reached for position (s' num2str(sPos) ',m' num2str(mPos) ')']); end
+
+        % Gain Nexus + RME
+        p_meas = p_meas/gain_nexus;
+
+        % Calculate RIR
+        rir = impzest(sweep,p_meas);
+
+        % Plot RIR
+        plot(t,rir), grid on
+        xlabel('Time /s'), title('Impulse response')
+
+        % Save pressure & RIR
+        fileName = [folderData fileNamePrefix 's' num2str(sPos,'%02.f') '_m' num2str(mPos,'%02.f')];
+        save(fileName,'rir','p_meas');
+        disp(['Data saved to ' fileName])
+    end
+
+    disp(['Done with measurements for Source Position ' num2str(sPos)])
+end
